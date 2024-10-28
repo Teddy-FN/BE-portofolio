45eca2b948ef03c9be6a39ae69c53876ec450175
Teddy-FN <tedd.ferdy@gmail.com>
2024-10-29 00:04:40 +0700

    Adding Config Production

--- config/config.js
+++ config/config.js
@@ -18,10 +18,17 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        require: "true",
+      },
+    },
+    timezone: "+07:00",
   },
 };
