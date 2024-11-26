32b0e60e681aa94e0739599f683e92ad44f3dbe2
Teddy-FN <tedd.ferdy@gmail.com>
2024-11-26 15:14:37 +0700

    CRUD Education

--- api/routes/education.js
+++ api/routes/education.js
@@ -5,10 +5,19 @@ const app = express.Router();
 // Controller
 const educationController = require("../controller/education");
 
+// Get List Education
 app.get("/get-education", educationController.getEducation);
 
+// Get By Id
+app.get("/get-education/:id", educationController.getEducationById);
+
+// Add New Education
 app.post("/add-education", educationController.postEducation);
 
+// Edit Education
 app.put("/edit-education/:id", educationController.editEducation);
+
+// Delete Education
+app.delete("/delete-education/:id", educationController.deleteEducationById);
 
 module.exports = app;
