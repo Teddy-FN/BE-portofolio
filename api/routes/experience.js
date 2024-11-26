791406771df5f7b8828a26210f3c237107f10a37
Teddy-FN <tedd.ferdy@gmail.com>
2024-11-26 13:55:43 +0700

    CRUD Experience

--- api/routes/experience.js
+++ api/routes/experience.js
@@ -5,10 +5,19 @@ const app = express.Router();
 // Controller
 const experienceController = require("../controller/experience");
 
+// Get List Experience
 app.get("/get-experience", experienceController.getExperience);
 
+// Get Experience By Id
+app.get("/get-experience/:id", experienceController.getExperienceById);
+
+// Create New Experience
 app.post("/add-experience", experienceController.postExperience);
 
+// Edit Experience
 app.put("/edit-experience/:id", experienceController.editExperience);
+
+// Delete
+app.delete("/delete-experience/:id", experienceController.deleteExperienceById);
 
 module.exports = app;
