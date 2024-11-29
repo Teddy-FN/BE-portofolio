28ea0d600d92adc0382ef7888213bace385b1776
Teddy-FN <tedd.ferdy@gmail.com>
2024-11-29 10:02:27 +0700

    Feature CRUD Skills

--- api/routes/skills.js
+++ api/routes/skills.js
@@ -5,10 +5,19 @@ const app = express.Router();
 // Controller
 const skillsController = require("../controller/skills");
 
+// Get Skills
 app.get("/get-skills", skillsController.getSkills);
 
+// Get Experience By Id
+app.get("/get-skills/:id", skillsController.getSkillsById);
+
+// Add Skills
 app.post("/add-skills", skillsController.postSkills);
 
+// Edit Skills
 app.put("/edit-skills/:id", skillsController.editSkills);
+
+// Delete
+app.delete("/delete-skills/:id", skillsController.deleteSkillsById);
 
 module.exports = app;
