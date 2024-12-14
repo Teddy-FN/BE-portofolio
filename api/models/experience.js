51206bf4def0c19fd9cbc8e4482abdf07bd8de47
Teddy-FN <tedd.ferdy@gmail.com>
2024-12-14 17:32:01 +0700

    Fixing Models Table Experience

--- api/models/experience.js
+++ api/models/experience.js
@@ -20,7 +20,7 @@ module.exports = sequelize.define(
       type: DataTypes.STRING,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     position: {
       type: DataTypes.STRING,
